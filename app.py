0965de89831d50504b7ffcc2a92504428ce9423b
coderbeez <nospam@firville.com>
2019-10-29 08:47:55 +0000

    Add get & post methods.

--- app.py
+++ app.py
@@ -10,46 +10,46 @@ app.secret_key = os.getenv('SECRET_KEY')
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegisterForm()
-    return render_template('register.html', form=form)
+    return render_template("register.html", form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    return render_template('login.html', form=form )
+    return render_template("login.html", form=form )
 
 
 @app.route("/notes")
 def notes():
-    return render_template('notes.html')
+    return render_template("notes.html")
 
 
-@app.route("/addnote")
+@app.route("/addnote", methods=["GET", "POST"])
 def addnote():
     form = AddNoteForm()
-    return render_template('addnote.html', form=form)
+    return render_template("addnote.html", form=form)
 
 
-@app.route("/editnote")
+@app.route("/editnote", methods=["GET", "POST"])
 def editnote():
-    return render_template('editnote.html')
+    return render_template("editnote.html")
 
 
-@app.route("/links")
+@app.route("/links", methods=["GET", "POST"])
 def links():
-    return render_template('links.html')
+    return render_template("links.html")
 
 
-@app.route("/addlink")
+@app.route("/addlink", methods=["GET", "POST"])
 def addlink():
     form = AddLinkForm()
-    return render_template('addlink.html', form=form) 
+    return render_template("addlink.html", form=form) 
 
 
 if __name__ == '__main__':
