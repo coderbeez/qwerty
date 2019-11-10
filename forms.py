102396ae8c2a5333464a33c82223582efded9627
coderbeez <nospam@firville.com>
2019-11-10 08:37:41 +0000

    Move PointForm before NoteForm to allow FormField.

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, PasswordField, SubmitField, SelectField, RadioField
+from wtforms import StringField, TextAreaField, PasswordField, SubmitField, SelectField, RadioField, FieldList, FormField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, InputRequired, URL
 
 class RegisterForm(FlaskForm):
@@ -14,18 +14,19 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=20)])
     submit = SubmitField('Login')
 
+class PointForm(FlaskForm):
+    ppoint_type = RadioField('Point Type', validators=[InputRequired()], choices=[('Description', 'Description'), ('Syntax', 'Syntax'), ('Know', 'Know'), ('Code', 'Code')])
+    ppoint_content= TextAreaField('Point Content', validators=[InputRequired()])
+    psubmit = SubmitField('Save')   
+
 class NoteForm(FlaskForm):
     language = RadioField('Language', validators=[InputRequired()], choices=[('HTML', 'HTML'), ('CSS', 'CSS'), ('JavaScript', 'JavaScript'), ('Python', 'Python')]) 
     topic = SelectField('Topic', validators=[InputRequired()], choices=[('', '--select--'),(' Fundamentals', ' Fundamentals'), ('Topic2', 'Topic2'), ('Topic3', 'Topic3')])
     name = StringField('Note Name', validators=[InputRequired()])   
+    points = FieldList(FormField(PointForm))
     point_type = RadioField('Point Type', validators=[InputRequired()], choices=[('Description', 'Description'), ('Syntax', 'Syntax'), ('Know', 'Know'), ('Code', 'Code')])
     point_content= TextAreaField('Point Content', validators=[InputRequired()])
     submit = SubmitField('Save')
-
-class PointForm(FlaskForm):
-    point_type = RadioField('Point Type', validators=[InputRequired()], choices=[('Description', 'Description'), ('Syntax', 'Syntax'), ('Know', 'Know'), ('Code', 'Code')])
-    point_content= TextAreaField('Point Content', validators=[InputRequired()])
-    submit = SubmitField('Save')    
 
 class LinkForm(FlaskForm):
     language = RadioField('Language', validators=[InputRequired()], choices=[('HTML', 'HTML'), ('CSS', 'CSS'), ('JavaScript', 'JavaScript'), ('Python', 'Python')]) 
