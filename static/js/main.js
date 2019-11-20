0ef0b7a4465d00e892480e0e7145cec5ee8fdc50
coderbeez <nospam@firville.com>
2019-11-20 12:12:00 +0000

    Remove time delays on submit due 2 bug.

--- static/js/main.js
+++ static/js/main.js
@@ -7,64 +7,95 @@ $(document).ready(function () {
     const star4 = $("[data-star=4]");
     const star5 = $("[data-star=5]");
     const rate = $("[data-star=rate]");
-    const submit = $("[data-star=submit]");
-    const form = $("[data-star=form]");
+    
+
+    const star_sub1 = $("[data-star=sub1]");
+    const star_sub2 = $("[data-star=sub2]");
+    const star_sub3 = $("[data-star=sub3]");
+    const star_sub4 = $("[data-star=sub4]");
+    const star_sub5 = $("[data-star=sub5]");
+    const flag = $("[data-flag=flag]");
+    const form = $("[data-submit=form]");
+    const submit1 = $("[data-submit=submit1]");
+    
     
 
 
-    star1.click(function () {
-        star1.css("color", "#00a9bd");
-        star2.css("color", "#c9c9c9");
-        star3.css("color", "#c9c9c9");
-        star4.css("color", "#c9c9c9");
-        star5.css("color", "#c9c9c9");
-        rate.val("1");
-       
+    //submit2.click(function () {
+        //star_sub1.css("color", "#00a9bd");
+        //star_sub2.css("color", "#c9c9c9");
+        //star_sub3.css("color", "#c9c9c9");
+        //star_sub4.css("color", "#c9c9c9");
+        //star_sub5.css("color", "#c9c9c9");
+        //setTimeout(function() {
+           //form.submit();
+      //},1000);      
+    //})
+
+    
+
+    flag.click(function () {
+        flag.css("color", "#ED5023");
+        //setTimeout(function() {
+            //form.submit();
+      //},1000);   
     })
 
-    star2.click(function () {
-        star1.css("color", "#00a9bd");
-        star2.css("color", "#00a9bd");
-        star3.css("color", "#c9c9c9");
-        star4.css("color", "#c9c9c9");
-        star5.css("color", "#c9c9c9");
-        rate.val("2");
-    })
+  
+  
 
-    star3.click(function () {
+  star1.click(function () {
+    star1.css("color", "#00a9bd");
+    star2.css("color", "#c9c9c9");
+    star3.css("color", "#c9c9c9");
+    star4.css("color", "#c9c9c9");
+    star5.css("color", "#c9c9c9");
+    rate.val("1"); 
+})
 
-        star1.css("color", "#00a9bd");
-        star2.css("color", "#00a9bd");
-        star3.css("color", "#00a9bd");
-        star4.css("color", "#c9c9c9");
-        star5.css("color", "#c9c9c9");
-        rate.val("3");
+star2.click(function () {
+    star1.css("color", "#00a9bd");
+    star2.css("color", "#00a9bd");
+    star3.css("color", "#c9c9c9");
+    star4.css("color", "#c9c9c9");
+    star5.css("color", "#c9c9c9");
+    rate.val("2");
+})
 
-    })
+star3.click(function () {
+    star1.css("color", "#00a9bd");
+    star2.css("color", "#00a9bd");
+    star3.css("color", "#00a9bd");
+    star4.css("color", "#c9c9c9");
+    star5.css("color", "#c9c9c9");
+    rate.val("3");
+})
 
-    star4.click(function () {
-        star1.css("color", "#00a9bd");
-        star2.css("color", "#00a9bd");
-        star3.css("color", "#00a9bd");
-        star4.css("color", "#00a9bd");
-        star5.css("color", "#c9c9c9");
-        rate.val("4");
-    })
+star4.click(function () {
+    star1.css("color", "#00a9bd");
+    star2.css("color", "#00a9bd");
+    star3.css("color", "#00a9bd");
+    star4.css("color", "#00a9bd");
+    star5.css("color", "#c9c9c9");
+    rate.val("4");
+})
 
-    star5.click(function () {
-        star1.css("color", "#00a9bd");
-        star2.css("color", "#00a9bd");
-        star3.css("color", "#00a9bd");
-        star4.css("color", "#00a9bd");
-        star5.css("color", "#00a9bd");
-        rate.val("5");
-    })
+star5.click(function () {
+    star1.css("color", "#00a9bd");
+    star2.css("color", "#00a9bd");
+    star3.css("color", "#00a9bd");
+    star4.css("color", "#00a9bd");
+    star5.css("color", "#00a9bd");
+    rate.val("5");
+})
+
+  
 
 
-submit.click(function() {
-    setTimeout(function() {
-        form.submit();
-  },1000);     
+
+
+
+ 
 });
 //WHERE: https://forum.jquery.com/topic/settimeout-before-form-submit
 
@@ -73,5 +104,4 @@ submit.click(function() {
 
 
 
-});
 
